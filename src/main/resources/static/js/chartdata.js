b2d27480d5f2213f42c0a3f6021543b1a623a9ff
Francois Polo <andasan@gmail.com>
2021-11-16 13:36:47 -0800

    fix chart and database hydration

--- src/main/resources/static/js/chartdata.js
+++ src/main/resources/static/js/chartdata.js
@@ -16,13 +16,9 @@ var myChart = new Chart(ctx, {
     data: {
         labels: labelData,
         datasets: [{
-            label: '# of Votes',
-            data: numericData,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-            ],
+            label: 'My First dataset',
+            backgroundColor: ["#1824b6", "#e56b7c", "#86a440", "#ef3fa3", "#ddee28"],
+            data: numericData
         }]
     },
     options: {
